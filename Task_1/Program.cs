b0c2b364e43a493f8e15a42b3f2468b5dd4ea6ee
caregor <caregor@gmail.com>
2022-07-10 02:04:47 +0300

    Task_1 Recursion

---
+++ Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+void CalcPositiveElements(int i = 0, int res = 0)
+{
+     
+    if (i >  0) 
+    {
+        res++;
+        System.Console.WriteLine("Total Positive Elements = " + res);
+        
+    }
+    i = Prompt("Enter a number: ");
+    CalcPositiveElements(i, res);
+}
+
+CalcPositiveElements();
